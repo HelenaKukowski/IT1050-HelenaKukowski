6d30e7630cd13b7ca469df6bb30b52cc2a20692d
Helena Kukowski <48935989+HelenaKukowski@users.noreply.github.com>
2019-04-28 09:15:55 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,32 @@
-﻿// A Hello World! program in c#.
+﻿//Fig. 3.18: Addition,cs
+//Displaying the sum of two numbers input from the keyboard.
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Lab01
+public class Addition
 {
-    class Program
+    //Main method begins execution of C# application
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World! From Helena Kukowski");
+        int number1; //declare first number to add
+        int number2; //declare second number to add
+        int sum; //delare sum of number1 and number2
+
+        Console.Write("Enter first integer:"); //prompt user
+        //read first number from user
+        number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second integer:"); //prompt user
+        //read second number from user
+        number2 = Convert.ToInt32(Console.ReadLine());
+
+        sum = number1 + number2; //add numbers
+        Console.WriteLine("Sum is {0}", sum); //display sum
+        Console.WriteLine("{0}\n{1}", "Hello World!", "From Helena Kukowski.");
+        Console.WriteLine("Hello World!\tFrom Helena Kukowski.");
         }
     }
-}
+    
