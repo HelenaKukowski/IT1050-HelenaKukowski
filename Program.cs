5c2076cabe0b9e356142ae3444228e7dfc169898
Helena Kukowski <48935989+HelenaKukowski@users.noreply.github.com>
2019-04-29 10:27:23 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,28 @@
-﻿//Fig. 3.18: Addition,cs
-//Displaying the sum of two numbers input from the keyboard.
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Addition
+namespace Lab06number07
 {
-    //Main method begins execution of C# application
-    public static void Main(string[] args)
+    class Program
     {
-        int number1; //declare first number to add
-        int number2; //declare second number to add
-        int sum; //delare sum of number1 and number2
+        static void Main(string[] args)
+        {
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
 
-        Console.Write("Enter first integer:"); //prompt user
-        //read first number from user
-        number1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Enter second integer:"); //prompt user
-        //read second number from user
-        number2 = Convert.ToInt32(Console.ReadLine());
-
-        sum = number1 + number2; //add numbers
-        Console.WriteLine("Sum is {0}", sum); //display sum
-        Console.WriteLine("{0}\n{1}", "Hello World!", "From Helena Kukowski.");
-        Console.WriteLine("Hello World!\tFrom Helena Kukowski.");
+           
+            foreach (string name in names)
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine("{0, 2}. {1}", i, names[i]);
+                    
+                }
+                Console.ReadLine();
+            }
+            
         }
     }
-    
+}
