658bc4924dbaee0fd18268f0d3abcc2d95677383
Helena Kukowski <48935989+HelenaKukowski@users.noreply.github.com>
2019-04-29 10:31:01 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab06number07
+namespace Lab0number02
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
-
-           
-            foreach (string name in names)
+            string[] months = new string[12];
             {
-                for (int i = 0; i < names.Length; i++)
+                months[0] = "January";
+                months[1] = "February";
+                months[2] = "March";
+                months[3] = "April";
+                months[4] = "May";
+                months[5] = "June";
+                months[6] = "July";
+                months[7] = "August";
+                months[8] = "September";
+                months[9] = "October";
+                months[10] = "November";
+                months[11] = "December";
+                for (int i = 0; i <= months.Length-1; i++)
                 {
-                    Console.WriteLine("{0, 2}. {1}", i, names[i]);
-                    
+                    Console.WriteLine("{0,2}. {1}", i, months[i]);
                 }
-                Console.ReadLine();
+
             }
-            
         }
     }
 }
